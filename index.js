d655f776dcef5cfcf54df8741c4ff83761ff04b9
ijTuhin <isratishu0209@gmail.com>
2023-01-18 16:22:21 +0600

    Database connected

--- index.js
+++ index.js
@@ -13,15 +13,22 @@ require('dotenv').config(); // For storing DB name & password security
 /* ********************
   DB connection & setup
   ******************** */
-const uri = "mongodb+srv://{process.env.DB_USER}:{process.env.DB_PASS}@cluster0.12dph.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("Database Connected");
-  client.close();
-});
 
+async function run() {
+  try{
+    const userCollection = client.db("messMealDB").collection("user"); // taking database & collection
+    const user = {emailId: "something", password: "something"}; // creating data 
+    const result = await userCollection.insertOne(user) // inserting data into the collection
+    console.log(result)
+  }
+  finally{
+    // here we can put code to end the connection.
+  }
+}
 
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log('Server running');
